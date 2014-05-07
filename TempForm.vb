c44229e6b3da762e34964ec270b318957f0d2dbf
JimGonsoski <james.gonsoski@metc.state.mn.us>
2014-05-07 15:50:56 -0500

    Clean out files leaving only those MME editor itself

--- TempForm.vb
+++
@@ -1,8 +0,0 @@
-Public Class TempForm
-
-    Private Sub TempForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        MasterController.frm = New EditorForm
-        MasterController.frm.Show(Me)
-    End Sub
-
-End Class
